6d3d85047f888a8774571020cda2e476211c511c
liangyangang <liangyangang@uniontech.com>
2025-09-08 09:11:42 +0800

    修改utpam_token函数

--- lib/libutpam/src/utpam_misc.rs
+++ lib/libutpam/src/utpam_misc.rs
@@ -8,6 +8,7 @@
 const DELIMITERS: &[char] = &[' ', '\t', '\n'];
 //从给定的字符串中提取标记
 pub fn utpam_tokenize<'a>(from: Option<&'a str>, next: &mut Option<&'a str>) -> Option<String> {
+    let mut token = String::new();
     let from = match from {
         Some(s) => s,
         None => match next {
@@ -17,7 +18,7 @@ pub fn utpam_tokenize<'a>(from: Option<&'a str>, next: &mut Option<&'a str>) -> 
     };
 
     // 找到第一个不在 DELIMITERS 中的字符，如果没找到返回字符长度
-    let start = from
+    let mut start = from
         .find(|c: char| !DELIMITERS.contains(&c))
         .unwrap_or(from.len());
 
@@ -27,24 +28,25 @@ pub fn utpam_tokenize<'a>(from: Option<&'a str>, next: &mut Option<&'a str>) -> 
         return None;
     }
 
-    //处理[...]
+    /*
+     * 处理[...]
+     * 将 [...] 中的内容作为一个整体，并去除方括号返回
+     * 如果有嵌套的情况，则只处理最外层的[],即[..[..]..]这种情况最终返回..[..]..
+     */
     let mut end = start;
     if from.chars().nth(start) == Some('[') {
         let mut to_escape = false;
-        let mut depth = 1; //[...]嵌套层数
-        for (i, c) in from.chars().enumerate().skip(start + 1) {
+        start += 1;
+
+        for (i, c) in from.chars().enumerate().skip(start) {
             if c == '\\' {
                 to_escape = true;
             } else if to_escape {
                 to_escape = false;
             } else if c == ']' {
-                depth -= 1;
-                if depth == 0 {
-                    end = i + 1;
-                    break;
-                }
-            } else if c == '[' {
-                depth += 1; //[..[..]..] 增加嵌套层数
+                token = from[start..i].to_string();
+                end = i + 1;
+                break;
             }
         }
     } else {
@@ -52,10 +54,8 @@ pub fn utpam_tokenize<'a>(from: Option<&'a str>, next: &mut Option<&'a str>) -> 
         end = from[start..]
             .find(|c: char| DELIMITERS.contains(&c))
             .map_or(from.len(), |pos| start + pos);
+        token = from[start..end].to_string();
     }
-
-    //从 from 中提取两个分隔符之间的子串
-    let token = from[start..end].to_string();
 
     // 更新 next
     if end < from.len() {
@@ -64,5 +64,5 @@ pub fn utpam_tokenize<'a>(from: Option<&'a str>, next: &mut Option<&'a str>) -> 
         *next = None;
     }
 
-    Some(token)
+    Some(token.to_string())
 }
